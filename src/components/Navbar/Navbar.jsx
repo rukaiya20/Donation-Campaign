981d6a7d4b2fd01ee9ec54ce6a7675544cce1ac6
unknown <rukaiyajahantajin239@gmail.com>
2024-04-23 21:43:41 +0600

    respinsive navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,42 @@
 import { NavLink } from 'react-router-dom';
-import Logo from '../../assets/Logo.png'
-
+import Logo from '../../assets/Logo.png';
+import './Navbar.css';
+import { TiThMenuOutline } from "react-icons/ti";
+import { IoIosCloseCircle } from "react-icons/io";
+import { useState } from 'react';
 const Navbar = () => {
-    return (
-        <nav className=' max-w-7xl flex justify-between mx-auto'>
-            <img src={Logo} alt="" />
-            <div className='space-x-20'>
-               <NavLink to={'/'}>Home</NavLink>
-               <NavLink to={'/donation'}>Donation</NavLink>
-               <NavLink to={'/statistics'}>Statistics</NavLink>
-            </div>
-        </nav>
+
+    const [open,setOpen] = useState(false);
+
+    return (<>
+    
+       <div className='flex md:justify-between items-center md:mt-12'>
+          <div>
+             <img className='w-44 md:ml-32 ml-4 md:mt-0 mt-2' src={Logo} alt="" />
+          </div>
+         <nav className='md:mr-32 ml-32'>       
+           
+           <div className='md:hidden text-2xl' onClick={() => setOpen(!open)}>
+                   {
+                     open === false ?
+                     <IoIosCloseCircle className='text-2xl'></IoIosCloseCircle> :
+                     <TiThMenuOutline className='text-2xl'></TiThMenuOutline>
+                   }
+           </div>
+            
+            <ul className={`md:flex md:gap-10 duration-1000 absolute md:static
+                   ${
+                    open ? '-top-80' : 'top-11'
+                   }    
+                       `}>
+                <li><NavLink to={'/'}>Home</NavLink></li>
+                <li><NavLink to={'/donation'}>Donation</NavLink></li>
+                <li><NavLink to={'/statistics'}>Statistics</NavLink></li>
+            </ul>
+         </nav>
+       </div>
+
+    </>
     );
 };
 
